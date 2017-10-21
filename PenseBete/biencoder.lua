6cf1eb6709d560df87c39fff76e863b6b60eb19c
Hieronims <jerome.enligne@laposte.net>
2017-10-21 11:31:46 +0200

    Create biencoder.lua

---
+++ PenseBete/biencoder.lua
@@ -0,0 +1,6 @@
+-- Les images
+
+local imgBG1  -- img pour image, puis infos sur l'image en question
+local bgX  -- bg pour background
+local musicCool -- music pour les musiques
+local sndCalme  -- snd pour les sons
